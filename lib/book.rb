edbba62bae466ab69328acf2de0def0bc88b1d0a
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-19 13:55:03 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,12 @@
 class Book
-attr_accessor :title
+attr_accessor :title, :author_name, :page_count, :genre 
 
   def initialize(title)
     @title = title
+  end
+  
+  def turn_page
+    
   end
 end 
 
